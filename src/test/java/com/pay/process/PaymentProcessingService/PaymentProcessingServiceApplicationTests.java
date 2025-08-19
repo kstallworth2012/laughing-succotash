a40bd6cc8f3c5a11c23c2b12f5733689e63995c6
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-08-19 18:28:20 -0400

    Initial Update

---
+++ src/test/java/com/pay/process/PaymentProcessingService/PaymentProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.process.PaymentProcessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
